7883713be71dc2ca0d6980014257aee44f1b54fa
dcy <766320183@qq.com>
2016-10-22 15:57:53 +0800

    eutil完善

--- src/eutil.erl
+++ src/eutil.erl
@@ -3,13 +3,22 @@
          md5_hex/1,
          to_list/1, to_binary/1,
          http_get/1, http_get/2, http_get/4,
-         http_post/3, http_post/4
+         http_post/3, http_post/4,
+         term_to_string/1, string_to_term/1,
+         term_to_bitstring/1, bitstring_to_term/1,
+         get_ets/2, put_ets/3, del_ets/2, get_ets_keys/1, get_ets_keys/2,
+         mapskeydelete/3, mapskeyreplace/4, mapskeyfind/3,
+         eval/2
         ]).
 
 -include("eutil.hrl").
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-define(FLOAT_BIAS, 1022).
+-define(MIN_EXP, -1074).
+-define(BIG_POW, 4503599627370496).
+
 -define(PERCENT, 37).  % $\%
 -define(FULLSTOP, 46). % $\.
 -define(QS_SAFE(C), ((C >= $a andalso C =< $z) orelse
@@ -25,7 +34,7 @@ quote_plus(Int) when is_integer(Int) ->
 quote_plus(Binary) when is_binary(Binary) ->
     quote_plus(binary_to_list(Binary));
 quote_plus(Float) when is_float(Float) ->
-    quote_plus(mochinum:digits(Float));
+    quote_plus(digits(Float));
 quote_plus(String) ->
     quote_plus(String, []).
 
@@ -41,6 +50,196 @@ quote_plus([C | Rest], Acc) ->
 
 hexdigit(C) when C < 10 -> $0 + C;
 hexdigit(C) when C < 16 -> $A + (C - 10).
+
+digits(N) when is_integer(N) ->
+    integer_to_list(N);
+digits(0.0) ->
+    "0.0";
+digits(Float) ->
+    {Frac1, Exp1} = frexp_int(Float),
+    [Place0 | Digits0] = digits1(Float, Exp1, Frac1),
+    {Place, Digits} = transform_digits(Place0, Digits0),
+    R = insert_decimal(Place, Digits),
+    case Float < 0 of
+        true ->
+            [$- | R];
+        _ ->
+            R
+    end.
+
+%% @spec int_pow(X::integer(), N::integer()) -> Y::integer()
+%% @doc  Moderately efficient way to exponentiate integers.
+%%       int_pow(10, 2) = 100.
+int_pow(_X, 0) ->
+    1;
+int_pow(X, N) when N > 0 ->
+    int_pow(X, N, 1).
+
+%% @spec int_ceil(F::float()) -> integer()
+%% @doc  Return the ceiling of F as an integer. The ceiling is defined as
+%%       F when F == trunc(F);
+%%       trunc(F) when F &lt; 0;
+%%       trunc(F) + 1 when F &gt; 0.
+int_ceil(X) ->
+    T = trunc(X),
+    case (X - T) of
+        Pos when Pos > 0 -> T + 1;
+        _ -> T
+    end.
+
+
+%% Internal API
+
+int_pow(X, N, R) when N < 2 ->
+    R * X;
+int_pow(X, N, R) ->
+    int_pow(X * X, N bsr 1, case N band 1 of 1 -> R * X; 0 -> R end).
+
+insert_decimal(0, S) ->
+    "0." ++ S;
+insert_decimal(Place, S) when Place > 0 ->
+    L = length(S),
+    case Place - L of
+         0 ->
+            S ++ ".0";
+        N when N < 0 ->
+            {S0, S1} = lists:split(L + N, S),
+            S0 ++ "." ++ S1;
+        N when N < 6 ->
+            %% More places than digits
+            S ++ lists:duplicate(N, $0) ++ ".0";
+        _ ->
+            insert_decimal_exp(Place, S)
+    end;
+insert_decimal(Place, S) when Place > -6 ->
+    "0." ++ lists:duplicate(abs(Place), $0) ++ S;
+insert_decimal(Place, S) ->
+    insert_decimal_exp(Place, S).
+
+insert_decimal_exp(Place, S) ->
+    [C | S0] = S,
+    S1 = case S0 of
+             [] ->
+                 "0";
+             _ ->
+                 S0
+         end,
+    Exp = case Place < 0 of
+              true ->
+                  "e-";
+              false ->
+                  "e+"
+          end,
+    [C] ++ "." ++ S1 ++ Exp ++ integer_to_list(abs(Place - 1)).
+
+
+digits1(Float, Exp, Frac) ->
+    Round = ((Frac band 1) =:= 0),
+    case Exp >= 0 of
+        true ->
+            BExp = 1 bsl Exp,
+            case (Frac =/= ?BIG_POW) of
+                true ->
+                    scale((Frac * BExp * 2), 2, BExp, BExp,
+                          Round, Round, Float);
+                false ->
+                    scale((Frac * BExp * 4), 4, (BExp * 2), BExp,
+                          Round, Round, Float)
+            end;
+        false ->
+            case (Exp =:= ?MIN_EXP) orelse (Frac =/= ?BIG_POW) of
+                true ->
+                    scale((Frac * 2), 1 bsl (1 - Exp), 1, 1,
+                          Round, Round, Float);
+                false ->
+                    scale((Frac * 4), 1 bsl (2 - Exp), 2, 1,
+                          Round, Round, Float)
+            end
+    end.
+
+scale(R, S, MPlus, MMinus, LowOk, HighOk, Float) ->
+    Est = int_ceil(math:log10(abs(Float)) - 1.0e-10),
+    %% Note that the scheme implementation uses a 326 element look-up table
+    %% for int_pow(10, N) where we do not.
+    case Est >= 0 of
+        true ->
+            fixup(R, S * int_pow(10, Est), MPlus, MMinus, Est,
+                  LowOk, HighOk);
+        false ->
+            Scale = int_pow(10, -Est),
+            fixup(R * Scale, S, MPlus * Scale, MMinus * Scale, Est,
+                  LowOk, HighOk)
+    end.
+
+fixup(R, S, MPlus, MMinus, K, LowOk, HighOk) ->
+    TooLow = case HighOk of
+                 true ->
+                     (R + MPlus) >= S;
+                 false ->
+                     (R + MPlus) > S
+             end,
+    case TooLow of
+        true ->
+            [(K + 1) | generate(R, S, MPlus, MMinus, LowOk, HighOk)];
+        false ->
+            [K | generate(R * 10, S, MPlus * 10, MMinus * 10, LowOk, HighOk)]
+    end.
+
+generate(R0, S, MPlus, MMinus, LowOk, HighOk) ->
+    D = R0 div S,
+    R = R0 rem S,
+    TC1 = case LowOk of
+              true ->
+                  R =< MMinus;
+              false ->
+                  R < MMinus
+          end,
+    TC2 = case HighOk of
+              true ->
+                  (R + MPlus) >= S;
+              false ->
+                  (R + MPlus) > S
+          end,
+    case TC1 of
+        false ->
+            case TC2 of
+                false ->
+                    [D | generate(R * 10, S, MPlus * 10, MMinus * 10,
+                                  LowOk, HighOk)];
+                true ->
+                    [D + 1]
+            end;
+        true ->
+            case TC2 of
+                false ->
+                    [D];
+                true ->
+                    case R * 2 < S of
+                        true ->
+                            [D];
+                        false ->
+                            [D + 1]
+                    end
+            end
+    end.
+
+unpack(Float) ->
+    <<Sign:1, Exp:11, Frac:52>> = <<Float:64/float>>,
+    {Sign, Exp, Frac}.
+
+transform_digits(Place, [0 | Rest]) ->
+    transform_digits(Place, Rest);
+transform_digits(Place, Digits) ->
+    {Place, [$0 + D || D <- Digits]}.
+
+
+frexp_int(F) ->
+    case unpack(F) of
+        {_Sign, 0, Frac} ->
+            {Frac, ?MIN_EXP};
+        {_Sign, Exp, Frac} ->
+            {Frac + (1 bsl 52), Exp - 53 - ?FLOAT_BIAS}
+    end.
 
 %% @spec urlencode([{Key, Value}]) -> string()
 %% @doc URL encode the property list.
@@ -127,3 +326,119 @@ http_post(URL, Headers, PayloadMaps, Options) when is_map(PayloadMaps) ->
 http_post(URL, Headers, PayloadItems, Options) when is_list(PayloadItems) ->
     Payload = list_to_binary(urlencode(PayloadItems)),
     http_post(URL, Headers, Payload, Options).
+
+
+%term_to_string(Term) ->
+%    %%  用下面这个方法虽然能保证 C S 这些字符比较好的入库，但会导致 下面问题
+%    %%1> binary_to_list(list_to_binary(io_lib:format("~p", [[59, 62, 230]]))).  -> "\";>鎈""
+%    %% 	binary_to_list(list_to_binary(io_lib:format("~p", [Term]))).
+%    %%use a more effecional way
+%    lists:flatten(io_lib:format("~w", [Term])).
+%% term序列化，term转换为string格式，e.g., [{a},1] => "[{a},1]"
+term_to_string(Term) ->
+    binary_to_list(list_to_binary(io_lib:format("~w", [Term]))).
+
+%% term序列化，term转换为bitstring格式，e.g., [{a},1] => <<"[{a},1]">>
+term_to_bitstring(Term) ->
+    erlang:list_to_bitstring(io_lib:format("~w", [Term])).
+
+%% term反序列化，string转换为term，e.g., "[{a},1]"  => [{a},1]
+string_to_term(String) ->
+    case erl_scan:string(String ++ ".") of
+        {ok, Tokens, _} ->
+            case erl_parse:parse_term(Tokens) of
+                {ok, Term} -> Term;
+                _Err -> undefined
+            end;
+        _Error ->
+            undefined
+    end.
+
+%% term反序列化，bitstring转换为term，e.g., <<"[{a},1]">>  => [{a},1]
+bitstring_to_term(undefined) -> undefined;
+bitstring_to_term(BitString) ->
+    string_to_term(binary_to_list(BitString)).
+
+
+%%************************************************
+%% keypos设置为1，set并且 get_ets 和 put_ets 配套使用
+get_ets(Ets, Key) ->
+    case ets:lookup(Ets, Key) of
+        [] -> undefined;
+        [{Key, Value}] -> Value
+    end.
+
+put_ets(Ets, Key, Value) ->
+    ets:insert(Ets, {Key, Value}).
+
+del_ets(Ets, Key) ->
+    ets:delete(Ets, Key).
+
+get_ets_keys(Tab) ->
+    ets:safe_fixtable(Tab, true),
+    Keys = get_ets_keys(Tab, [], ets:first(Tab)),
+    ets:safe_fixtable(Tab, false),
+    Keys.
+
+get_ets_keys(Tab, Fun) ->
+    ets:safe_fixtable(Tab, true),
+    Keys = get_ets_keys(Fun, Tab, [], ets:first(Tab)),
+    ets:safe_fixtable(Tab, false),
+    Keys.
+
+
+get_ets_keys(_Tab, Keys, '$end_of_table') ->
+    Keys;
+get_ets_keys(Tab, Keys, Key) ->
+    NextKey = ets:next(Tab, Key),
+    get_ets_keys(Tab, [Key | Keys], NextKey).
+
+get_ets_keys(_Fun, _Tab, Keys, '$end_of_table') ->
+    Keys;
+get_ets_keys(Fun, Tab, Keys, Key) ->
+    NextKey = ets:next(Tab, Key),
+    case Fun(Key) of
+        true -> get_ets_keys(Fun, Tab, [Key | Keys], NextKey);
+        false -> get_ets_keys(Fun, Tab, Keys, NextKey)
+    end.
+
+%%************************************************
+
+%%util:eval("B + A.",[{'A', 0}, {'B', 2}]).
+eval(ExprStr, Environ) ->
+    BindFun = fun({Arg, Val}, Bindings) ->
+        erl_eval:add_binding(Arg, Val, Bindings)
+    end,
+    NewBindings = lists:foldl(BindFun, erl_eval:new_bindings(), Environ),
+
+    {ok, Scanned, _} = erl_scan:string(ExprStr),
+    {ok, Parsed} = erl_parse:parse_exprs(Scanned),
+    {value, Result, _NewBindings} = erl_eval:exprs(Parsed, NewBindings),
+    Result.
+
+%%%%%%%%%%%%%%%%%%%%%%Maps%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mapskeydelete(What, Key, [H|T]) ->
+	case maps:get(Key, H) == What of
+		true -> T;
+		false -> [H|mapskeydelete(What, Key, T)]
+	end;
+mapskeydelete(_, _, []) -> [].
+
+mapskeyreplace(What, Key, L, New) when is_list(L), erlang:is_map(New) ->
+	mapskeyreplace3(What, Key, L, New).
+
+mapskeyreplace3(What, Key, [H|T], New) ->
+	case maps:get(Key, H) == What of
+		true -> [New|T];
+		false -> [H|mapskeyreplace3(What, Key, T, New)]
+	end;
+mapskeyreplace3(_, _, [], _) -> [].
+
+mapskeyfind(_What, _Key, []) ->
+	false;
+mapskeyfind(What, Key, [H|T]) ->
+	case maps:get(Key, H) == What of
+		true -> H;
+		false -> mapskeyfind(What, Key, T)
+	end.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
